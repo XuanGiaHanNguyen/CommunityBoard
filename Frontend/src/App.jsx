722d43ff85d5c9cfd070627d602e0cf84985851e
XuanGiaHanNguyen <han316@usf.edu>
2025-06-10 23:16:16 +0700

    added more course

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -34,6 +34,16 @@ function App() {
             <Card name="System Design 101" image={eight} tag="Video" link="https://www.youtube.com/watch?v=uw-gcK9bjkk"/>
             <Card name="Basics of UI/UX Designs" image={nine} tag="Course" link="https://www.youtube.com/watch?v=c9Wg6Cb_YlU&t=4269s"/>
             <Card name="Pathway to Fintech" image={ten} tag="Documents" link="https://ondemand.euromoney.com/discover/pathways/fintech-essentials"/>
+             <Card name="Introduction to Programming" image={one} tag="Course" link="https://ocw.mit.edu/collections/introductory-programming/"/>
+            <Card name="Technical Interview Prep" image={two} tag="Course" link="https://www.codepath.org/courses/tech-interview-prep/"/>
+            <Card name="Web development 101" image={three} tag="Course" link="https://www.codepath.org/courses/web-development"/>
+            <Card name="Software Engineering Roadmap" image={four} tag="RoadMap" link="https://roadmap.sh/"/>
+            <Card name="Introduction to AI" image={five} tag="Course" link="https://pll.harvard.edu/course/cs50s-introduction-artificial-intelligence-python"/>
+            <Card name="Cloud Management" image={six} tag="Document" link="https://cloud.google.com/learn/what-is-cloud-management"/>
+            <Card name="Data Structure and Algorithm" image={seven} tag="Document" link="https://www.w3schools.com/dsa/dsa_intro.php"/>
+            <Card name="System Design 101" image={eight} tag="Video" link="https://www.youtube.com/watch?v=uw-gcK9bjkk"/>
+            <Card name="Basics of UI/UX Designs" image={nine} tag="Course" link="https://www.youtube.com/watch?v=c9Wg6Cb_YlU&t=4269s"/>
+            <Card name="Pathway to Fintech" image={ten} tag="Documents" link="https://ondemand.euromoney.com/discover/pathways/fintech-essentials"/>
         </div>
       </div>
     </div>
